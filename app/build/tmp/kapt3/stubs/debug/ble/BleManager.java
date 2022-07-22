f341b213ad3a29122864fe0c9057584235cefe95
Agnesh S Kumar <agnesh.skumar@gadgeon.com>
2022-07-22 16:13:12 +0530

    commit 22-7-2022-1

---
+++ app/build/tmp/kapt3/stubs/debug/ble/BleManager.java
@@ -0,0 +1,27 @@
+package ble;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u0000\u001a\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0000\b\u00c6\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u0006\u0010\t\u001a\u00020\nR\u001c\u0010\u0003\u001a\u0004\u0018\u00010\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0005\u0010\u0006\"\u0004\b\u0007\u0010\b\u00a8\u0006\u000b"}, d2 = {"Lble/BleManager;", "", "()V", "device", "Landroid/bluetooth/BluetoothDevice;", "getDevice", "()Landroid/bluetooth/BluetoothDevice;", "setDevice", "(Landroid/bluetooth/BluetoothDevice;)V", "initialize", "", "app_debug"})
+public final class BleManager {
+    @org.jetbrains.annotations.NotNull()
+    public static final ble.BleManager INSTANCE = null;
+    @org.jetbrains.annotations.Nullable()
+    private static android.bluetooth.BluetoothDevice device;
+    
+    private BleManager() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    public final android.bluetooth.BluetoothDevice getDevice() {
+        return null;
+    }
+    
+    public final void setDevice(@org.jetbrains.annotations.Nullable()
+    android.bluetooth.BluetoothDevice p0) {
+    }
+    
+    public final void initialize() {
+    }
+}
---
+++ app/build/tmp/kapt3/stubs/debug/ble/DeviceFragment.java
@@ -0,0 +1,34 @@
+package ble;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u0000>\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J$\u0010\u000f\u001a\u00020\u00102\u0006\u0010\u0011\u001a\u00020\u00122\b\u0010\u0013\u001a\u0004\u0018\u00010\u00142\b\u0010\u0015\u001a\u0004\u0018\u00010\u0016H\u0016R\u001a\u0010\u0005\u001a\u00020\u0006X\u0086.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0007\u0010\b\"\u0004\b\t\u0010\nR\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u000b\u001a\u00020\fX\u0082.\u00a2\u0006\u0002\n\u0000R\u000e\u0010\r\u001a\u00020\u000eX\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0017"}, d2 = {"Lble/DeviceFragment;", "Landroidx/fragment/app/Fragment;", "ble", "Lquevedo/soares/leandro/blemadeeasy/BLE;", "(Lquevedo/soares/leandro/blemadeeasy/BLE;)V", "binding", "Lcom/example/poc/databinding/FragmentDeviceBinding;", "getBinding", "()Lcom/example/poc/databinding/FragmentDeviceBinding;", "setBinding", "(Lcom/example/poc/databinding/FragmentDeviceBinding;)V", "device", "Landroid/bluetooth/BluetoothDevice;", "viewModel", "Lble/BleViewModel;", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
+public final class DeviceFragment extends androidx.fragment.app.Fragment {
+    private final quevedo.soares.leandro.blemadeeasy.BLE ble = null;
+    public com.example.poc.databinding.FragmentDeviceBinding binding;
+    private ble.BleViewModel viewModel;
+    private android.bluetooth.BluetoothDevice device;
+    
+    public DeviceFragment(@org.jetbrains.annotations.NotNull()
+    quevedo.soares.leandro.blemadeeasy.BLE ble) {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.example.poc.databinding.FragmentDeviceBinding getBinding() {
+        return null;
+    }
+    
+    public final void setBinding(@org.jetbrains.annotations.NotNull()
+    com.example.poc.databinding.FragmentDeviceBinding p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public android.view.View onCreateView(@org.jetbrains.annotations.NotNull()
+    android.view.LayoutInflater inflater, @org.jetbrains.annotations.Nullable()
+    android.view.ViewGroup container, @org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+        return null;
+    }
+}
