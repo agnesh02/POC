1b0f784930712bab5f3c83da8fd0aa52e433325f
Agnesh S Kumar <agnesh.skumar@gadgeon.com>
2022-07-26 10:56:38 +0530

    commit 26-7-2022-1

--- app/src/main/java/ble/DeviceFragment.kt
+++ app/src/main/java/ble/DeviceFragment.kt
@@ -11,11 +11,11 @@ import com.example.poc.databinding.FragmentDeviceBinding
 
 class DeviceFragment : Fragment() {
 
+//all readable characteristics for sample smartwatch
+//[00002a00-0000-1000-8000-00805f9b34fb, 00002a01-0000-1000-8000-00805f9b34fb, 00002a04-0000-1000-8000-00805f9b34fb, 00002aa6-0000-1000-8000-00805f9b34fb]
+
 //readable: 00001800-0000-1000-8000-00805f9b34fb: 00002a00-0000-1000-8000-00805f9b34fb (device name)
 //writeable: 0000f618-0000-1000-8000-00805f9b34fb: 0000b002-0000-1000-8000-00805f9b34fb
-
-//all readable characteristics
-//[00002a00-0000-1000-8000-00805f9b34fb, 00002a01-0000-1000-8000-00805f9b34fb, 00002a04-0000-1000-8000-00805f9b34fb, 00002aa6-0000-1000-8000-00805f9b34fb]
 
     private lateinit var binding: FragmentDeviceBinding
     private lateinit var viewModel: BleViewModel
@@ -38,6 +38,8 @@ class DeviceFragment : Fragment() {
 
         binding.btnFragDeviceWriteData.setOnClickListener {
             //BLE.writeData
+            //val data = "A50419" + Integer.toHexString((System.currentTimeMillis() / 1000).toInt())
+            //val value = BLE.writeData(device,"service","f000ee07-0451-4000-b000-000000000000",data)
         }
 
         binding.btnFragDeviceDisconnect.setOnClickListener {
